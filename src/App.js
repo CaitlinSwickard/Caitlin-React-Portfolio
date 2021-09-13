c1eafbe0b7af0784394e0ccc15df5a61bcd5fa27
Caitlin Swickard <caitlin6029@gmail.com>
2021-09-12 19:02:15 -0600

    building navbar/home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to my portfolio'
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content"></div>
-      <h1>{title}</h1>
+      <Home />
     </div >
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return ( 
+    <nav className='navbar'>
+      <h1>Caitlin Swickard</h1>
+      <div className='links'>
+        <a herf="/">Home</a>
+        <a herf="/projects">Projects</a>
+        <a herf="/about">About</a>
+        <a herf="/contact">Contact</a>
+        <a herf="/resume">Resume</a>
+      </div>
+    </nav>
+   );
+}
+ 
+export default Navbar;

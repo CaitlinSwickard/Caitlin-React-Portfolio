91e07b292e8556bae3a37525e32d0d36ef867ef4
Caitlin Swickard <caitlin6029@gmail.com>
2021-09-16 16:01:22 -0600

    created about me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,6 +2,10 @@ import React from 'react';
 import '../../App.css';
 
 export default function About() {
-  return <h1 className="about-me">About Me</h1>;
-
+  return (
+    <>
+      <h1 className="about-me">About Me</h1>
+      <p>Full-stack web developer dedicated to building and optimizing user-centric websites. Recently earned a certificate in full stack development from the University of Denver with newly developed skills in JavaScript, CSS, Node.js, and responsive web design.</p>
+    </>
+  )
 }
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,22 +5,24 @@ export default function Contact() {
   return (
     <>
       <h1 className="contact">Contact me</h1>
-      <form>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" required />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" required />
-      </div>
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" required />
-      </div>
-      <button type="submit"></button>
-    </form>
-    <button>Submit</button>
+      <container className="contact-container">
+        <form>
+          <div>
+            <label className="name">Name:</label>
+            <input type="text" id="name" required />
+          </div>
+          <div>
+            <label className="email">Email:</label>
+            <input type="email" id="email" required />
+          </div>
+          <div>
+            <label className="message">Message:</label>
+            <textarea id="message" required />
+          </div>
+          {/* <button type="submit"></button> */}
+        </form>
+        <button className="form-btn">Submit</button>
+      </container>
     </>
   )
 

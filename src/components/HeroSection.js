a3c57c8e48939254747ac774dd554a07eb16626c
Caitlin Swickard <caitlin6029@gmail.com>
2021-09-16 11:33:17 -0600

    Created hero section for Home page

---
+++ src/components/HeroSection.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../App.css'
+import { Button } from './Button';
+import './HeroSection.css';
+
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <img src="images/yellowcupbackground.jpeg" alt="image of computer and yellow cup"/>
+      <h1>Caitlin Swickard</h1>
+      <p>Full Stack Web Developer</p>
+      <div className="hero-btns">
+        <Button className="btns"
+        buttonStyle="btn--outline"
+        buttonSize="btn--large"
+        >
+          My Work
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { Button } from './Button';
@@ -11,6 +11,8 @@ const Navbar = () => {
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  // sizing for mobile responsive button
   const showButton = () => {
     if (window.innerWidth <= 960) {
       setButton(false);
@@ -18,48 +20,60 @@ const Navbar = () => {
       setButton(true);
     }
   };
+  // to hide button on refresh
+  useEffect(() => {
+    showButton();
+  }, []);
 
   window.addEventListener('resize', showButton);
 
   return (
     <>
-      <nav className="navbar navbar">
-        <div className="navbar-container">
-          <Link to='/' className="navbar-logo">
-            <i class="fas fa-code"></i>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          {/* to close mobile menu on click */}
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            Home <i className='fas fa-code' />
           </Link>
-          <div className="menu-icon" onClick={handleClick}>
-            <i className={click ? 'fa fa-times' : 'fa fa-bars'} />
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
-          <ul className={click ? 'nav-menu-active' : 'nav-menu'}>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className='nav-item'>
-              <Link to='/' className='nav-link' onClick={closeMobileMenu}>
-                Home
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link to='/projects' className='btn-mobile' onClick={closeMobileMenu}>
-                Projects
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link to='/about' className='nav-link' onClick={closeMobileMenu}>
+              <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
                 About
               </Link>
             </li>
             <li className='nav-item'>
-              <Link to='/resume' className='nav-link' onClick={closeMobileMenu}>
+              <Link
+                to='/resume'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
                 Resume
               </Link>
             </li>
             <li className='nav-item'>
-              <Link to='/contact' className='nav-link' onClick={closeMobileMenu}>
+              <Link
+                to='/contact'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
                 Contact
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to='/Projects'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                PROJECTS
               </Link>
             </li>
           </ul>
           {button && <Button buttonStyle='btn--outline'>PROJECTS</Button>}
-
         </div>
       </nav>
     </>

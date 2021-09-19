48460d004221b78a4ce1c0aeca757f143d61fce5
Caitlin Swickard <caitlin6029@gmail.com>
2021-09-19 17:21:57 -0600

    completed about me page

---
+++ public/images/components/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../../App.css';
+import { Container } from '@material-ui/core';
+import Footer from '../Footer';
+
+export default function About() {
+  return (
+    <>
+      <h1 className="about-me">About Me</h1>
+      <Container className="info">
+        <p>Full-stack web developer dedicated to building and optimizing user-centric websites.
+        Recently earned a certificate in full stack development from the University of Denver
+        with newly developed skills in JavaScript, CSS, Node.js, and responsive web design.</p>
+        <img className="head-shot" src="/images/caitlin-headshot.png" alt="head-shot"></img>
+      </Container>
+
+      <Footer />
+    </>
+  )
+}

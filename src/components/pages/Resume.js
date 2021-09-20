319562d66428833dad3c744383420694c1bfc616
Caitlin Swickard <caitlin6029@gmail.com>
2021-09-19 18:31:37 -0600

    added styling to resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,11 +3,9 @@ import '../../App.css';
 import { Container } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
 import Footer from '../Footer';
 
 
@@ -20,14 +18,14 @@ export default function Resume() {
           <Grid item xs={12} md={6} lg={4}>
             <Card sx={{ maxWidth: 400 }} className='skills-card'>
               <CardContent className='card-content'>
-                <Typography variant='h5' className='title'> 
+                <Typography variant='h5' className='title'>
                   Programming Languages
               </Typography>
-              <br></br>
-                <Typography>
-                  <p>HTML5</p>
-                  <p>CSS3</p>
-                  <p>JavaScript ES6+</p>
+                <br></br>
+                <Typography >
+                  <p className='skills'>HTML5</p>
+                  <p className='skills'>CSS3</p>
+                  <p className='skills'>JavaScript ES6+</p>
                   <br></br>
                   <br></br>
                   <br></br>
@@ -42,12 +40,12 @@ export default function Resume() {
                 <Typography variant='h5' className='title'>
                   Front End
               </Typography>
-              <br></br>
-                <Typography>
-                  <p>Material Ui</p>
-                  <p>Bootstrap</p>
-                  <p>jQuery</p>
-                  <p>Bulma</p>
+                <br></br>
+                <Typography >
+                  <p className='skills'>Material Ui</p>
+                  <p className='skills'>Bootstrap</p>
+                  <p className='skills'>jQuery</p>
+                  <p className='skills'>Bulma</p>
                   <br></br>
                   <br></br>
                 </Typography>
@@ -61,19 +59,23 @@ export default function Resume() {
                 <Typography variant='h5' className='title'>
                   Back End
               </Typography>
-                <Typography>
-                  <p>Node.js</p>
-                  <p>Express.js</p>
-                  <p>React</p>
-                  <p>MongoDB</p>
-                  <p>MySQL</p>
-                  <p>NoSQl</p>
-                  <p>API</p>
+                <Typography >
+                  <p className='skills'>Node.js</p>
+                  <p className='skills'>Express.js</p>
+                  <p className='skills'>React</p>
+                  <p className='skills'>MongoDB</p>
+                  <p className='skills'>MySQL</p>
+                  <p className='skills'>NoSQl</p>
+                  <p className='skills'>API</p>
                 </Typography>
               </CardContent>
             </Card>
           </Grid>
         </Grid>
+      </Container>
+      <Container className='resume-container'>
+        <h2 className='h2'>Download my resume</h2>
+        <button className="resume-btn">Click Here!</button>
       </Container>
 
       <Footer />
